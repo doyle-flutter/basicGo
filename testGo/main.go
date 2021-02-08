21c90242c2453f74d9d980530f69b455ab0e2962
doyle <il4479@hanmail.net>
2021-02-08 09:59:40 +0900

    router

--- testGo/main.go
+++ testGo/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Println(req.Host)
+		fmt.Println(req.URL.Path)
+		fmt.Println(req.URL.Query())
 		res.Write([]byte("James Dev"))
 	})
+	http.HandleFunc("/my/", myHandle)
 	http.ListenAndServe(":8000", nil)
+}
+
+func myHandle(res http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL.Path)
+	fmt.Println(req.URL.Query())
+	res.Write([]byte("MY - James Dev"))
 }
